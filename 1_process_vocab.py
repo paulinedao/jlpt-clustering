6ab4d38691ac961e9320820adb878dddb2b19ad2
Pauline Dao <paulined917@gmail.com>
2026-01-15 18:43:17 +0100

    Add processing step of japanese words

---
+++ 1_process_vocab.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def main(input_csv=None, output_csv=None):
+    input_csv = 'data/jlpt_vocab.csv'
+    output_csv = 'data/jlpt_vocab_n5.csv'
+    process_vocab(input_csv, output_csv)
+    
+def process_vocab(input_csv, output_csv):
+    (
+        pd.read_csv(input_csv)
+        .drop(columns=['Furigana', 'English'], errors='ignore')
+        .query("`JLPT Level` == 'N5'")
+        .rename(columns={'Original': 'Word'})
+        .to_csv(output_csv, index=False)
+    )
+    
+if __name__ == "__main__":
+    main()
