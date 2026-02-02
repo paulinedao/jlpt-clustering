f650603c0150c4296a342cad854cc38668367ccc
Pauline Dao <paulined917@gmail.com>
2026-02-02 22:59:27 +0100

    Refactor to implement Snakemake

--- 7_hdbscan_cluster.py
+++ 7_hdbscan_cluster.py
@@ -1,11 +1,11 @@
 import numpy as np
 import hdbscan
 
-INPUT_EMB = "data/jlpt_embeddings_umap_15d.npy"
-OUTPUT_LABELS = "data/jlpt_cluster_labels.npy"
+#input_emb = "data/jlpt_embeddings_umap_15d.npy"
+#output_labels = "data/jlpt_cluster_labels.npy"
 
-def main():
-    X = np.load(INPUT_EMB)
+def main(input_emb, output_labels):
+    X = np.load(input_emb)
 
     clusterer = hdbscan.HDBSCAN(
         min_cluster_size=10,
@@ -14,11 +14,11 @@ def main():
     )
 
     labels = clusterer.fit_predict(X)
-    np.save(OUTPUT_LABELS, labels)
-
+    np.save(output_labels, labels)
     n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
     print("Clusters found:", n_clusters)
     print("Noise points:", (labels == -1).sum())
 
 if __name__ == "__main__":
-    main()
+    main(input_emb=snakemake.input.input_emb, 
+         output_labels=snakemake.output.output_labels)
