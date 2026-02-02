9e2928635678d99ccba1867e33b93702e1235606
Pauline Dao <paulined917@gmail.com>
2026-02-02 22:58:47 +0100

    Refactor to implement Snakemake

--- 3_tokenize.py
+++ 3_tokenize.py
@@ -2,11 +2,13 @@
 import pandas as pd
 import json
 
-INPUT_CSV = "data/jlpt_words_pos_filtered.csv"
-OUTPUT_JSON = "data/jlpt_tokens.json"
+#INPUT_CSV = "data/jlpt_words_pos_filtered.csv"
+#OUTPUT_JSON = "data/jlpt_tokens.json"
 
-def main():
-    tokenize_words(INPUT_CSV, OUTPUT_JSON)
+def main(input_csv=None, output_json=None):
+    #input_csv = "data/jlpt_words_pos_filtered.csv"
+    #output_csv = "data/jlpt_tokens.json"
+    tokenize_words(input_csv, output_json)
 
 def tokenize_words(input_csv, output_json):
     # Load words
@@ -23,4 +25,5 @@ def tokenize_words(input_csv, output_json):
     print(f"Saved {len(tokens)} tokenized entries to {output_json}")
 
 if __name__ == "__main__":
-    main()
+    main(input_csv=snakemake.input.input_csv, 
+         output_json=snakemake.output.output_json)
